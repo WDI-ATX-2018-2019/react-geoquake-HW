2e34e559585e6a8a1bccd05009c9b97c5629a22c
Tristan Bennett <tristanbennett@tristanova.local>
2019-02-19 11:39:21 -0600

    updated css to make more responsive

--- src/App.js
+++ src/App.js
@@ -39,11 +39,11 @@ class App extends Component {
             center={this.state.center}
             zoom={this.state.zoom}
           />
+          <p id="attr">Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>, licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a></p>
         </div>
         <div className="quakeContainer">
           <h1>Significant earthquakes from the past 30 days: </h1>
           <EarthquakeList earthquakes={this.state.earthquakes} />
-          <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>, licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a></div>
         </div>
       </div>
     );
